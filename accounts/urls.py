caf8148067be5ded3272e1ecae0a21f156ba81e8
wellington.ideao <wellington.ideao@gmail.com>
2018-11-19 16:29:54 -0200

    Seção 4 - Usuários - Template do painel do usuário

--- accounts/urls.py
+++ accounts/urls.py
@@ -20,7 +20,9 @@ app_name = 'accounts'
 # urlpatterns contém a lista de roteamentos de URLs
 urlpatterns = [
     #GET /
+    path('', accounts_views.dashboard, name='dashboard'),
     path('entrar', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('sair', auth_views.LogoutView.as_view(next_page='website:home'), name='logout'),
     path('cadastra-se', accounts_views.register, name='register'),
 
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,29 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.conf import settings
+from . forms import RegisterForm
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+
+
+@login_required
+def dashboard(request):
+    template_name = 'accounts/dashboard.html'
+    return render(request, template_name)
 
 
 def register(request):
     template_name = 'accounts/register.html'
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = RegisterForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect(settings.LOGIN_URL)
+            user = form.save()
+            user = authenticate(
+                username=user.name, password=form.cleaned_data['password1']
+            )
+            login(request, user)
+            return redirect(settings.LOGIN_REDIRECT_URL)
     else:
-        form = UserCreationForm()
+        form = RegisterForm()
     context = {'form': form}
     return render(request, template_name, context)
 
